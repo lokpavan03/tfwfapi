b06c9a7315e34a80582027a035b5f7d86c78c558
LOK <80965262+lokpavan03@users.noreply.github.com>
2021-04-27 12:25:50 +0530

    Update org.sh

--- org.sh
+++ org.sh
@@ -2,8 +2,8 @@
 
 # 1. Define Variables
 $ curl \
-  --header "Authorization: Bearer $TOKEN" \
+  --header "Authorization: Bearer $Token" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
   --data @payload.json \
-  https://app.terraform.io/api/v2/organizations/lok/workspaces
+  https://app.terraform.io/api/v2/organizations/lok/workspaces-1
